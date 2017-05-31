b54ef72efe4c4f7bf10a07338691bbdefb62e3e8
Wilkinson D. de Souza <wilkinson@outlook.com.br>
2017-05-31 02:22:38 -0300

    Finish front end contact

--- js/contacts.js
+++ js/contacts.js
@@ -7,7 +7,33 @@
     }
   }
 
+  function loadFormEvent() {
+    var formulario = document.getElementById('contact');
+
+    function createNewContact(evento) {
+      var e = evento || window.event;
+      e.preventDefault();
+
+      var appurl = 'https://contact-track.herokuapp.com/contacts.json';
+      var dataAtual = new Date();
+      var data = {
+        "trackid": localStorage.getItem('trackid'),
+        "name": document.getElementById('nome').value,
+        "email": document.getElementById('email').value,
+      }
+      var request = new XMLHttpRequest();
+      request.open('POST', appurl);
+      request.withCredentials = true;
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.send(JSON.stringify(data));
+      alert('Seu contato foi cadastrado com sucesso.');
+      location.reload();
+    }
+    addEvent(formulario, 'submit', createNewContact);
+  }
+
   function sendPageView() {
+    var appurl = 'https://contact-track.herokuapp.com/pages.json';
     var dataAtual = new Date();
     var data = {
       "visitedurl": window.location.href,
@@ -15,29 +41,13 @@
       "accessdate": dataAtual.toISOString()
     }
 
-    function createCORSRequest(method, url) {
-      var xhr = new XMLHttpRequest();
-      if ("withCredentials" in xhr) {
-        // XHR for Chrome/Firefox/Opera/Safari.
-        xhr.open(method, url, true);
-      } else if (typeof XDomainRequest != "undefined") {
-        // XDomainRequest for IE.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-      } else {
-        // CORS not supported.
-        xhr = null;
-      }
-      return xhr;
-    }
-
-    var appurl = 'http://localhost:3000/pages.json';
-    var request = createCORSRequest("POST", appurl);
+    var request = new XMLHttpRequest();
+    request.open('POST', appurl);
     request.withCredentials = true;
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify(data));
   }
-
   setTrackingInfo();
   addEvent(window, 'load', sendPageView);
+  addEvent(window, 'load', loadFormEvent);
 }());
